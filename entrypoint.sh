f1e6376e8fb6dd1fe1a43dfb86d223c9f8fb3e7a
Cristian Franco <cristian_franko@hotmail.com>
2025-10-22 04:22:54 -0300

    entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
 
-# entrypoint.sh 
+# entrypoint.sh
 
-# Asegurar los permisos correctos en cada inicio
+# 1. Crear las carpetas de framework que Laravel necesita pero que no están en Git.
+mkdir -p storage/framework/sessions
+mkdir -p storage/framework/views
+mkdir -p storage/framework/cache/data
+
+# 2. Asegurar los permisos correctos en todas las carpetas necesarias en cada inicio.
 chown -R www-data:www-data storage bootstrap/cache
 
-# Ejecutar las optimizaciones de producción de Laravel
+# 3. Ejecutar las optimizaciones de producción de Laravel.
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
-# Ejecutar las migraciones de la base de datos
+# 4. Ejecutar las migraciones de la base de datos.
 php artisan migrate --force
 
-# Iniciar Supervisor para gestionar Nginx y PHP-FPM
+# 5. Iniciar Supervisor para gestionar Nginx y PHP-FPM.
 exec "$@"
